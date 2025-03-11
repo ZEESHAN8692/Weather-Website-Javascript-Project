95ba12b9e2aa03d8a069fc5576def0b6afa33911
ZEESHAN8692 <162082741+ZEESHAN8692@users.noreply.github.com>
2025-03-11 14:07:48 +0530

    class and prectice

--- js/script.js
+++ js/script.js
@@ -3,10 +3,13 @@ let searchBtn = document.querySelector(".searchBtn");
 
 const apiKey = "011221a2bcf1ba2efb623f1dd53cb91e";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+let hello =
+  "https://api.openweathermap.org/data/2.5/weather?q=bareilly&appid=b4c441239amshf0e17cc540a2f32p176f59js";
 // &appid=
 
 async function checkWeather(city) {
-  const response = await fetch(`${apiUrl}${"bareilly"}&appid=${apiKey}`);
+  const response = await fetch(hello);
+  // const response = await fetch(`${apiUrl}${"bareilly"}&appid=${apiKey}`);
   const data = await response.json();
   console.log(data);
   //   document.querySelector(".teplature").innerHTML = "";
