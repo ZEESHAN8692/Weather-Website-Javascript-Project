941d43f3dd71457f69298cfe6563b65825ec913d
ZEESHAN8692 <162082741+ZEESHAN8692@users.noreply.github.com>
2025-03-11 16:36:37 +0530

    Project Complete

--- js/script.js
+++ js/script.js
@@ -1,22 +1,46 @@
 let searchInput = document.querySelector(".searchInput");
 let searchBtn = document.querySelector(".searchBtn");
 
-const apiKey = "011221a2bcf1ba2efb623f1dd53cb91e";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
-let hello =
-  "https://api.openweathermap.org/data/2.5/weather?q=bareilly&appid=b4c441239amshf0e17cc540a2f32p176f59js";
-// &appid=
+const apiKey = "d8e84a6b367d4b6da5e95420251103";
+const apiUrl = "http://api.weatherapi.com/v1/current.json?key=";
+// let cityName = "bareilly";
 
 async function checkWeather(city) {
-  const response = await fetch(hello);
+  const response = await fetch(`${apiUrl}${apiKey}&q=${city}`);
   // const response = await fetch(`${apiUrl}${"bareilly"}&appid=${apiKey}`);
   const data = await response.json();
   console.log(data);
-  //   document.querySelector(".teplature").innerHTML = "";
-  //   document.querySelector(".cityName").innerHTML = "";
-  //   document.querySelector(".Humidity").innerHTML = "";
-  //   document.querySelector(".windSpeed").innerHTML = "";
+  document.querySelector(".teplature").innerHTML = `${Math.floor(
+    data.current.temp_c
+  )}°`;
+  document.querySelector(".cityName").innerHTML = data.location.name;
+  document.querySelector(".Humidity").innerHTML = `${data.current.humidity} %`;
+  document.querySelector(
+    ".windSpeed"
+  ).innerHTML = `${data.current.wind_kph} km/h`;
+  document.querySelector(".weatherText").innerHTML =
+    data.current.condition.text;
+  let WeatherImage = document.querySelector(".WeatherImage");
+  if (data.current.condition.text === "Mist") {
+    WeatherImage.src = "Static/mist.png";
+  } else if (
+    data.current.condition.text === "Clear" ||
+    data.current.condition.text === "Sunny"
+  ) {
+    WeatherImage.src = "Static/clear.png";
+  } else if (data.current.condition.text === "Clouds") {
+    WeatherImage.src = "Static/cloudy.png";
+  } else if (data.current.condition.text === "Drizzle") {
+    WeatherImage.src = "Static/drizzle.png";
+  } else if (data.current.condition.text === "Rain") {
+    WeatherImage.src = "Static/rainy.png";
+  } else if (data.current.condition.text === "Snow") {
+    WeatherImage.src = "Static/snow.pngs";
+  } else if (data.current.condition.text === "Fog") {
+    WeatherImage.src = "Static/fog.png";
+  }
 }
 
-searchBtn.addEventListener("click", () => {});
-checkWeather();
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchInput.value);
+});
