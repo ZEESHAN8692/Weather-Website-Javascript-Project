c10dc86403fbec602a8584100faa5b2da55a6fc1
ZEESHAN KHAN <162082741+ZEESHAN8692@users.noreply.github.com>
2025-03-19 21:07:03 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let searchInput = document.querySelector(".searchInput");
 let searchBtn = document.querySelector(".searchBtn");
 
 const apiKey = "d8e84a6b367d4b6da5e95420251103";
-const apiUrl = "http://api.weatherapi.com/v1/current.json?key=";
+const apiUrl = "https://api.weatherapi.com/v1/current.json?key=";
 // let cityName = "bareilly";
 
 async function checkWeather(city = "kolkata") {
